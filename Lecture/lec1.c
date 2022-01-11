f1f1f6a24ddd7cbcad14e25a865dc4671b7d5309
Youssef-Rachad <69124029+Youssef-Rachad@users.noreply.github.com>
2022-01-10 19:47:00 -0500

    Lab0

---
+++ Lecture/lec1.c
@@ -0,0 +1,37 @@
+// ESC190 Lecture: 1
+// Date : 10/01/2021
+
+// single line comments
+
+/* and multiline
+ * comments */
+
+#include <stdio.h> // <-- importing libraries
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv){ //<-- function is required to run this file
+  
+  return 0;
+}
+
+/* Functions have a return type declared
+ * parameters have declared types also
+ * semicolons are needed
+ * all functions return according to return type except for void funcs
+ */
+int function(int parameter){
+    return 0;
+}
+
+// compile with gcc filename.c -o output
+
+/* some datatypes
+* char   | 1 byte  | %c
+* int    | 4 bytes | %d
+* float  | 4 bytes | %f 
+* double | 8 bytes | %f more precise
+* -> formatting %.2f for truncation
+*/
+
+
---
+++ labs/Lab1.c
@@ -0,0 +1,40 @@
+// Question 1
+// 3456_10 = 1101 1000 0000_2
+// 2048+
+// 1024+
+//  256+
+//  128
+
+// Question 2
+// ./some_dir
+// ~/u/d/utorid/Documents/some_dir
+//
+// it runs ls .
+// Documents
+// |
+// |--> some_dir
+// |
+// |--> another_dir
+// |
+// |--> third_dir
+//      |
+//      |--> inside_dir
+//
+// Line 14 is the same as 15
+// Line 16 prints the parent directory list
+// ls and pwd dont change current directory
+//
+
+// Question 3
+// gcc filename.c -o output.out
+// ./output.out
+// ./third_dir/output.out
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
+
+
+
